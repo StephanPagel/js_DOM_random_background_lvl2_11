7771da21390ad0403a5c2a2e1fb9b3d738c3d093
StephanPagel <stephan.pagel84@gmail.com>
2022-05-11 09:36:23 +0200

    fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
 let body = document.getElementById('body');
 
 function generate() {
-    let r = Math.floor(Math.random() * 257);
-    let g = Math.floor(Math.random() * 257);
-    let b = Math.floor(Math.random() * 257);
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
     console.log(r);
     console.log(g);
     console.log(b);
